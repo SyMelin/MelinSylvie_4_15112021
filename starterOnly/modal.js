0975199cc92418c662295d6ffff3e65d36dd24ab
Sylvie Melin <sy.melin@gmail.com>
2021-11-23 20:38:11 +0100

    Avancées sur nombre de concours

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,43 +26,54 @@ close.addEventListener("click", function() {
    document.querySelector(".bground").style.display = "none";
 })
 
+
+//Validation des champs du formulaire
+
+// Création de l'élément content le message d'erreur
+
 let errorMessage = document.createElement("p");
-//errorMessage.textContent = "";
 errorMessage.style.color = "#FFF000";
 errorMessage.style.fontSize = "1rem";
-//document.querySelector(".formData").appendChild(errorMessage);
  
 
-/*
-let lastName = document.querySelector("#last");
-let lastNameParent = lastName.parentElement;
-lastName.addEventListener("change", function(e) {
-  console.log(lastName.value);
-  if (lastName.value.length < 2) {
-    errorMessage.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-    lastNameParent.appendChild(errorMessage);
-  } else if (lastName.value.length >= 2 && lastNameParent.lastElementChild == errorMessage) {
-    lastNameParent.removeChild(errorMessage);
-  };
-})
-*/
+//Validation sur lastName
 
-let lastName = document.querySelector("#last");
+let lastName = document.getElementById("last");
 let lastNameParent = lastName.parentElement;
 const lastNameMinLength = 2;
 lastName.addEventListener("change", function(e) {
-  console.log(lastName.value);
   if (lastName.value.length < lastNameMinLength) {
     errorMessage.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
     lastNameParent.appendChild(errorMessage);
-  } else if (lastName.value.length >= 2 && lastNameParent.lastElementChild == errorMessage) {
+  } else if (lastName.value.length >= lastNameMinLength && lastNameParent.lastElementChild == errorMessage) {
     lastNameParent.removeChild(errorMessage);
   };
-})
+});
+
+/*
+let lastName = document.getElementById("last");
+const lastNameMinLength = 2;
+let condition = lastName.value.length < lastNameMinLength;
+let condition2 = "lastName.value.length >= lastNameMinLength";
+let checkVal = function () {
+  if (condition) {
+    errorMessage.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
+    lastName.parentElement.appendChild(errorMessage);
+  } else if (condition2 && lastName.parentElement.lastElementChild == errorMessage) {
+    lastName.parentElement.removeChild(errorMessage);
+  };
+};
+
+lastName.addEventListener("change", function(e) {
+  checkVal();
+});
+*/
 
 
 
-let email = document.querySelector("#email");
+//Validation de email
+
+let email = document.getElementById("email");
 let emailParent = email.parentElement;
 email.addEventListener("change", function(e) {
   if (email.validity.typeMismatch) {
@@ -71,7 +82,50 @@ email.addEventListener("change", function(e) {
   } else if (!email.validity.typeMismatch && emailParent.lastElementChild == errorMessage) {
     emailParent.removeChild(errorMessage);
   };
-})
+});
+
+
+
+// Validation de quantity
+
+let quantity = document.getElementById("quantity");
+let quantityParent = quantity.parentElement;
+document.querySelector("form").addEventListener("submit", function (e) {
+    if (quantity.value === "") {
+      errorMessage.textContent = "Veuillez un nombre entre 0 et 99";
+      quantityParent.appendChild(errorMessage);
+      e.preventDefault();
+    }
+});
+
+
+/*
+let quantity = {
+  identifiant: document.getElementById("quantity"),
+  condition: 'quantity.value === ""',
+  errorText: "Veuillez un nombre entre 0 et 99",
+  valid: false
+};
+
+
+  let checkValidity = function (field) {
+    let fieldParent = field.identifiant.parentElement;
+    if (field.condition) {
+      errorMessage.textContent = field.errorText;
+      fieldParent.appendChild(errorMessage);
+    }
+  };
+
+  
+document.querySelector("form").addEventListener("submit", function (e) {
+  checkValidity (quantity);
+  if (quantity.valid === false) {
+    console.log(quantity.valid);
+    e.preventDefault;
+  }
+});
+*/
+
 
 // Checkbox1
 document.querySelector("form").addEventListener("submit", function(e) {
@@ -80,6 +134,6 @@ document.querySelector("form").addEventListener("submit", function(e) {
   if (checkbox1.checked === false) {
     errorMessage.textContent = "Vous devez vérifier que vous acceptez les termes et conditions";
     checkbox1Parent.appendChild(errorMessage);
-  e.preventDefault();
+    e.preventDefault();
   }
 });

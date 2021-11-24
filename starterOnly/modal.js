041e9a89bfdd30bdd96c035fd1735107bb5ffdaf
Sylvie Melin <sy.melin@gmail.com>
2021-11-24 20:46:12 +0100

    Validation du formulaire global

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,35 +34,8 @@ close.addEventListener("click", function() {
    document.querySelector(".bground").style.display = "none";
 })
 
-
-// Création de l'élément contenant le message d'erreur
-
-let errorMessage = document.createElement("p");
-errorMessage.style.color = "#FFF000";
-errorMessage.style.fontSize = "0.875rem";
+let formDataValidity = 0;
  
-
-// Fonctions associées au mesage d'erreur
-
-let addErrorMessage = function (field) {
-  let fieldParent = field.parentElement;
-  errorMessage.textContent = field.errorText;
-  fieldParent.appendChild(errorMessage);
-};
-
-let removeErrorMessage = function (field) {
-  let fieldParent = field.parentElement;
-  fieldParent.removeChild(errorMessage);
-};
-
-// On ajoute par défaut une classe notValid à chaque formData
-/*
-const formDataAll = document.getElementsByClassName("formData");
-for (let formData of formDataAll){
-  formData.classList.add("notValid");
-}
-*/
-
 // Validation sur firstName
 
 const firstName = document.getElementById("first");
@@ -73,14 +46,18 @@ firstName.parentElement.setAttribute("data-error", "Veuillez entrer " + firstNam
 let checkValidityText = function (element) {
   if (element.value.length < element.minLength) {
     element.parentElement.setAttribute("data-error-visible", "true");
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
+    return 1;
   };
 };
 
+let validityFirstName = 0;
 
 firstName.addEventListener("change", function(e) {
-  checkValidityText(firstName);
+  validityFirstName = checkValidityText(firstName);
+  console.log("validityFirstName " + validityFirstName);
   console.log("message "+ e.target.parentElement.getAttribute("data-error-visible"));
 });
 
@@ -91,37 +68,38 @@ const lastName = document.getElementById("last");
 lastName.minLength = 2;
 lastName.parentElement.setAttribute("data-error", "Veuillez entrer " + lastName.minLength + " caractères ou plus pour le champ du nom de famille");
 
+let validityLastName = 0;
 
 lastName.addEventListener("change", function(e) {
-  checkValidityText(lastName);
+  validityLastName = checkValidityText(lastName);
+  console.log("validityLastName " + validityLastName);
   console.log("message "+ e.target.parentElement.getAttribute("data-error-visible"));
 });
 
-let formDataValidity = 0;
+
 
 //Validation de email
 
 const email = document.getElementById("email");
 email.parentElement.setAttribute("data-error", "Veuillez entrer une adresse e-mail valide");
 //email.parentElement.setAttribute("data-error-visible", "false");
-console.log(email.parentElement.attributes);
+//console.log(email.parentElement.attributes);
 
 let checkValidityEmail = function(element) {
-  let count;
   if (element.validity.typeMismatch || element.value == "") {
     element.parentElement.setAttribute("data-error-visible", "true");
-    count = 0;
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
-    count = 1;
+    return 1;
   };
-  formDataValidity = formDataValidity + count;
-  console.log("fdValidity " + formDataValidity);
 };
 
+let validityEmail = 0;
 
 email.addEventListener("change", function(e) {
-  checkValidityEmail(email);
+  validityEmail = checkValidityEmail(email);
+  console.log("validityEmail " + validityEmail);
   console.log("message " + e.target.parentElement.getAttribute("data-error-visible"));
 });
 
@@ -136,14 +114,18 @@ birthdate.parentElement.setAttribute("data-error", "Veuillez entrer votre date d
 let checkValidityValue = function (element) {
   if (element.value === "") {
     element.parentElement.setAttribute("data-error-visible", "true");
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
+    return 1;
   };
 };
   
+let validityBirthdate = 0;
 
 birthdate.addEventListener("change", function (e) {
-  checkValidityValue(birthdate);
+  validityBirthdate = checkValidityValue(birthdate);
+  console.log("validityBirthdate " + validityBirthdate);
   console.log("message " + e.target.parentElement.getAttribute("data-error-visible"));
 });
 
@@ -158,16 +140,18 @@ let checkValidityRange = function (element) {
   if (element.value < element.min || element.value > 99) // 99 correspond à element.max mais la condition ne fonctionne pas en utilisant element.max (??!)
   {
     element.parentElement.setAttribute("data-error-visible", "true");
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
+    return 1;
   };
 };
 
-console.log(quantity.value);
+let validityQuantity = 0;
 
 quantity.addEventListener("change", function (e) {
-  checkValidityRange(quantity);
-  console.log(quantity.value);
+  validityQuantity = checkValidityRange(quantity);
+  console.log("validityQuantity " + validityQuantity);
   console.log("message " + e.target.parentElement.getAttribute("data-error-visible"));
 });
 
@@ -182,17 +166,28 @@ locations.parentElement.setAttribute("data-error","Veuillez choisir une option p
 let checkValidityRadio = function (element){
   if (element.checked === false) {
     element.parentElement.setAttribute("data-error-visible", "true");
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
+    return 1;
   };
 };
+
+let validityRadio = 0;
 
 for (let city of locationsAll) {
   city.addEventListener("change", function (e) {
     checkValidityRadio(city);
     console.log("message " + e.target.parentElement.getAttribute("data-error-visible"));
-  }
-)};
+    console.log(checkValidityRadio(city));
+    if (checkValidityRadio(city) == 1){
+      validityRadio = 1;
+      console.log("validityRadio " + validityRadio);
+    };
+  });
+};
+
+
 
 
 
@@ -203,27 +198,27 @@ checkbox1.parentElement.setAttribute("data-error", "Vous devez vérifier que vou
 checkbox1.parentElement.setAttribute("data-error-visible", "false");
 
 
-let validity = true;
+
 
 let checkValidityCheckbox = function (element) {
   if (element.checked === false) {
     element.parentElement.setAttribute("data-error-visible", "true");
-    validity = false;
+    return 0;
   } else {
     element.parentElement.setAttribute("data-error-visible", "false");
-    validity = true;
+    return 1;
   };
 };
 
-
+let validityCheckbox1 = 1;
 
 checkbox1.addEventListener("change", function(e) {
-  checkValidityCheckbox(checkbox1);
- //console.log("checkbox1Message " + checkbox1.parentElement.getAttribute("data-error-visible"));
-  console.log ("validity" + validity);
+  validityCheckbox1 = checkValidityCheckbox(checkbox1);
+  console.log("validityCheckbox1 " + validityCheckbox1);
+  console.log("message " + e.target.parentElement.getAttribute("data-error-visible"));
 });
 
-
+/*
 document.querySelector("form").addEventListener("submit", function(e) {
   checkValidityCheckbox(checkbox1);
   if (validity === false) {
@@ -231,43 +226,40 @@ document.querySelector("form").addEventListener("submit", function(e) {
   };
   console.log(checkbox1.parentElement.getAttribute("data-error-visible"));
 });
-
-
-
-//Validation du formulaire
-/*
-let validity;
-let validate = function () {
-  let compteurSubmit = 0;
-  for (let formData of formDataAll) {
-    if (formData.classList.contains("notValid")) {
-      compteurSubmit++;
-    };
-  };
-  console.log(formDataAll);
-  console.log(compteurSubmit);
-  if (compteurSubmit !== 0) {
-    checkValidityText(firstName);
-    checkValidityText(lastName);
-    checkValidityEmail(email);
-    checkValidityValue(birthdate);
-    checkValidityValue(quantity);
-    checkValidityRadio();
-    checkValidityCheckbox();
-    return validity = false;
-  };
-};
 */
 
-/*
-document.querySelector("form").addEventListener("submit", function(e) {
-  validate ();
-  console.log(validity);
-  if (validity = false) {
-    e.preventDefault();
-    validate();
-  } else {
-    alert ("Merci ! Votre réservation a été reçue.");
-  };
-  console.log(formDataAll);
-});*/
+//Validation du formulaire
+
+let validate = function () {
+return formDataValidity = 
+    validityFirstName +
+    validityLastName +
+    validityEmail +
+    validityBirthdate +
+    validityQuantity +
+    validityRadio +
+    validityCheckbox1;
+};
+
+    document.querySelector("form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      console.log (validityFirstName);
+      console.log (validityLastName);
+      console.log (validityEmail);
+      console.log (validityBirthdate);
+      console.log (validityQuantity);
+      console.log (validityRadio);
+      console.log (validityCheckbox1);
+      validate();
+      let formDataAll = document.getElementsByClassName("formData")
+      if (validate() === formDataAll.length){
+        console.log("BRAVO!");
+      } else {
+        console.log("NOPE!");
+          for (let formData of formDataAll) {
+            if (formData.getAttribute("data-error-visible") === null) {
+              formData.setAttribute("data-error-visible", "true");
+            };
+          };
+        };
+    });

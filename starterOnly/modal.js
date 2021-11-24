77162fa8ac81c4c3fb8a023bfb17169c77818bd8
Sylvie Melin <sy.melin@gmail.com>
2021-11-24 02:59:29 +0100

    Avancées sur firstname, birthdate, form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,7 +20,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////// CODE AJOUTE /////////////////////////
 
 // close modal form
 const close = document.querySelector(".content .close");
@@ -28,8 +28,17 @@ close.addEventListener("click", function() {
    document.querySelector(".bground").style.display = "none";
 })
 
+//Chaque formDatareçoit une class .notValid par défaut
+const formDataAll = document.getElementsByClassName("formData");
+for (let formData of formDataAll) {
+  formData.classList.add('notValid');
+}
+//console.log(formDataAll[2].classList);
 
-//Validation des champs du formulaire
+
+
+
+//Validation de chaque champ du formulaire
 
 // Création de l'élément content le message d'erreur
 
@@ -41,12 +50,33 @@ let addErrorMessage = function (field) {
   let fieldParent = field.parentElement;
   errorMessage.textContent = field.errorText;
   fieldParent.appendChild(errorMessage);
-}
+};
 
 let removeErrorMessage = function (field) {
   let fieldParent = field.parentElement;
   fieldParent.removeChild(errorMessage);
-}
+};
+
+
+const firstName = document.getElementById("first");
+first.errorText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
+first.minLength = 2;
+
+firstName.addEventListener("change", function(e) {
+  e.stopPropagation();
+  if (e.target.value.length < firstName.minLength) {
+    addErrorMessage(e.target);
+  } else if (e.target.parentElement.lastElementChild == errorMessage) {
+    removeErrorMessage (e.target);
+    e.target.parentElement.classList.remove('notValid');
+  } else {
+    e.target.parentElement.classList.remove('notValid');
+  };
+  console.log(e.target.parentElement.classList);
+});
+
+
+
 
 
 //Validation sur lastName
@@ -66,14 +96,19 @@ lastName.addEventListener("change", function(e) {
 
 const lastName = document.getElementById("last");
 lastName.errorText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-const lastNameMinLength = 2;
+lastName.minLength = 2;
 
 lastName.addEventListener("change", function(e) {
-  if (lastName.value.length < lastNameMinLength) {
-    addErrorMessage(lastName);
-  } else if (lastName.value.length >= lastNameMinLength && lastName.parentElement.lastElementChild == errorMessage) {
-    removeErrorMessage (lastName);
+  e.stopPropagation();
+  if (e.target.value.length < lastName.minLength) {
+    addErrorMessage(e.target);
+  } else if (e.target.parentElement.lastElementChild == errorMessage) {
+    removeErrorMessage (e.target);
+    e.target.parentElement.classList.remove('notValid');
+  } else {
+    e.target.parentElement.classList.remove('notValid');
   };
+  console.log(e.target.parentElement.classList);
 });
 
 
@@ -107,12 +142,33 @@ const email = document.getElementById("email");
 email.errorText = "Veuillez entrer une adresse e-mail valide";
 
 email.addEventListener("change", function(e) {
+  e.stopPropagation();
   if (email.validity.typeMismatch) {
     addErrorMessage (email);
-  } else if (!email.validity.typeMismatch && email.parentElement.lastElementChild == errorMessage) {
+  } else if (email.parentElement.lastElementChild == errorMessage) {
     removeErrorMessage (email);
-  };
+    e.target.parentElement.classList.remove('notValid');
+  } else {
+    e.target.parentElement.classList.remove('notValid');
+  }
+  console.log(e.target.parentElement.classList);
 });
+
+
+// Validation de birthdate
+
+const birthdate = document.getElementById("birthdate");
+birthdate.errorText = "Veuillez entrer votre date de naissance";
+
+document.querySelector("form").addEventListener("submit", function (e) {
+    if (birthdate.value === "") {
+      addErrorMessage (birthdate);
+      e.preventDefault();
+    } else {
+      birthdate.parentElement.classList.remove('notValid');
+    };
+    console.log(birthdate.parentElement.classList);
+}); 
 
 
 
@@ -125,27 +181,34 @@ document.querySelector("form").addEventListener("submit", function (e) {
     if (quantity.value === "") {
       addErrorMessage (quantity);
       e.preventDefault();
-    }
+    } else {
+      quantity.parentElement.classList.remove('notValid');
+    };
+    console.log(quantity.parentElement.classList);
 });
 
 // Validation pour bouton radio
 
-const locationsLabels = document.getElementsByClassName("checkbox-label");
-const locations = locationsLabels[0];
+const locationsAll = document.querySelectorAll("input[name='location']");
+const locations = locationsAll[0];
 locations.errorText = "Veuillez choisir une option parmi les villes proposées"
 
 document.querySelector("form").addEventListener("submit", function (e) {
   let compteur = 0;
-  for (let city of locationsLabels) {
-    if (city.previousElementSibling.checked) {
+  for (let city of locationsAll) {
+    if (city.checked) {
       compteur++;
     };
   };
+  console.log("compteur1 "+ compteur);
   if (compteur == 0) {
-    console.log("compteur "+ compteur);
+    console.log("compteur2 "+ compteur);
     addErrorMessage (locations);
       e.preventDefault();
+  } else {
+    locations.parentElement.classList.remove('notValid');
   };
+  console.log(locations.parentElement.classList);
 });
 
 
@@ -186,5 +249,26 @@ document.querySelector("form").addEventListener("submit", function(e) {
   if (checkbox1.checked === false) {
     addErrorMessage (checkbox1);
     e.preventDefault();
-  }
+  } else {
+    checkbox1.parentElement.classList.remove('notValid');
+  };
+  console.log(checkbox1.parentElement.classList);
+});
+
+//Validation du formulaire
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  let compteurSubmit = 0;
+  for (let formData of formDataAll) {
+    if (formData.classList.contains("notValid")) {
+      compteurSubmit++;
+    };
+  };
+  console.log(compteurSubmit);
+  if (compteurSubmit !== 0) {
+     e.preventDefault();
+  } else {
+    alert ("Merci ! Votre réservation a été reçue.");
+  };
+  console.log(formDataAll);
 });
